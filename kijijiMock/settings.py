8272583b808fa84271ac3c8d7f3e37be5a2c4a5e
Shawn Leblanc <Shawn.L99@hotmail.com>
2020-12-08 22:10:07 -0500

    setting change

--- kijijiMock/settings.py
+++ kijijiMock/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR /'static'/ 'authentication'/ #. os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = BASE_DIR /'static'/ 'authentication' #. os.path.join(BASE_DIR,'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/authentication'
 
 
 
