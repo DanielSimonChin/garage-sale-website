23caf8933ff1e2a57559d7475a42fd32e80c9690
unknown <danielsimonchin@hotmail.com>
2020-12-09 18:30:30 -0500

    made changes in settings.py

--- kijijiMock/settings.py
+++ kijijiMock/settings.py
@@ -15,7 +15,8 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -141,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT =  os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT =  os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/'
 
